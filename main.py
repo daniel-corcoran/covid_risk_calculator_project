d7b59bc3eed58e91b8240c8baf0e732c82f20ab6
daniel-corcoran <corcordp@mail.uc.edu>
2020-08-04 20:44:58 -0400

    Added house form

--- main.py
+++ main.py
@@ -3,26 +3,70 @@ from waitress import serve
 from flask import render_template, request
 import os
 
-# pip3 install flask
-# pip3 install waitress
-
-
 os.environ['GLOG_minloglevel'] = '2'
-@app.route('/submit', methods=['POST'])
-def proc_pst():
+
+
+@app.route('/submit_house', methods=['POST'])
+def proc_house():
+    print(request.form)
+
+    address = request.form['address']
+    roommate_1 = request.form['member1']
+    roommate_2 = request.form['member2']
+    roommate_3 = request.form['member3']
+    roommate_4 = request.form['member4']
+
+    try:
+        covid_1 = bool(request.form['covid-1'] == 'on')
+    except:
+        covid_1 = False
+
+    try:
+        covid_2 = bool(request.form['covid-2'] == 'on')
+    except:
+        covid_2 = False
+
+    try:
+        covid_3 = bool(request.form['covid-3'] == 'on')
+    except:
+        covid_3 = False
+
+    try:
+        covid_4 = bool(request.form['covid-4'] == 'on')
+    except:
+        covid_4 = False
+
+    house = {'address': address,
+             'roommate 1': {'name': roommate_1, 'covid': covid_1},
+             'roommate 2': {'name': roommate_2, 'covid': covid_2},
+             'roommate 3': {'name': roommate_3, 'covid': covid_3},
+             'roommate 4': {'name': roommate_4, 'covid': covid_4}}
+
+    print("-- house details --")
+    for member in house:
+        print(member, house[member])
+    print("-- end house details --")
+    return render_template('base.html', msg="User has been added to database")
+
+
+@app.route('/submit_friends', methods=['POST'])
+def proc_friends():
     text = request.form['text']
     try:
         (chb := request.form['covid'])
         chb = True
     except:
         chb = False
+
     print(text, chb)
+
     return render_template('base.html', msg="User has been added to database")
 
 
 @app.route('/')
 def open_fn():
     return render_template('base.html')
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8000)
